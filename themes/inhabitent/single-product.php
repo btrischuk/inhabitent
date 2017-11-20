f7623e6980a4d03d0ca3970db6056adedacdad1a
btrischuk <b-trischuk@hotmail.com>
2017-11-20 15:19:19 -0800

    replace single-products folder with single-product folder, style single.scss page

--- themes/inhabitent/single-product.php
+++ themes/inhabitent/single-product.php
@@ -9,13 +9,19 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-    <div class="testing">
 
-		<?php while ( have_posts() ) : the_post(); ?>
-				<?php 
+    <?php while ( have_posts() ) : the_post(); ?>
+    <!-- <div class="testing"> -->
+
+		<?php 
 			get_template_part( 'template-parts/content', 'single' ); 
-			?>
-
+		?>
+    <p class="single-product-social">
+      <button><a href="https://facebook.com"><i class="fa fa-facebook" aria-hidden="true"></i><span>Like</span></a></button>
+      <button><a href="https://twitter.com"><i class="fa fa-twitter" aria-hidden="true"></i><span>Tweet</span></a></button>
+      <button><a href="https://google-plus.com"><i class="fa fa-pinterest" aria-hidden="true"></i><span>Pin</span></a></button>
+    </p>
+<!-- </div>  -->
 			<?php the_post_navigation(); ?>
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template.
