c92ff172f6ecf00b25863bbf02e5d94cc37c13b7
btrischuk <b-trischuk@hotmail.com>
2017-11-21 18:30:15 -0800

    style search result page

--- themes/inhabitent/search.php
+++ themes/inhabitent/search.php
@@ -15,11 +15,36 @@ get_header(); ?>
 			<header class="page-header">
 				<h1 class="page-title"><?php printf( esc_html( 'Search Results for: %s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 			</header><!-- .page-header -->
+			<div class="testing">
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'template-parts/content', 'search' ); ?>
+
+
+
+
+				<?php 
+					the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+				?>
+		
+				<?php if ( 'post' === get_post_type() ) : ?>
+				<div class="entry-meta">
+					<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
+				</div><!-- .entry-meta -->
+				<?php endif; ?>
+			</header><!-- .entry-header -->
+		
+			<div class="entry-content">
+				<?php the_excerpt(); ?>
+			</div><!-- .entry-content -->
+		</article><!-- #post-## -->
+
+
+
+
+
+
 
 			<?php endwhile; ?>
 
@@ -33,6 +58,7 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</section><!-- #primary -->
+	</div>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
