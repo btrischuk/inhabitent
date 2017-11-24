bff1efd0fa29ab7a8d281937d1cda8d408d202e0
btrischuk <b-trischuk@hotmail.com>
2017-11-24 14:25:46 -0800

    fix js for search bar

--- themes/inhabitent/js/search.js
+++ themes/inhabitent/js/search.js
@@ -1,15 +1,41 @@
 (function ($) {
+  
+  $('.main-navigation #search-field').hide();
 
-  $('.main-navigation .search-field').hide();
 
-  $('.search-submit').click(function () {
-    // e.preventDefault()
-    $('.search-field').toggle('slow', function () {
-      $('.search-field').focus();
+  $('#search-submit').click(function() {
+    $('#search-field').toggle( 'slow', function(){
+    $('#search-field').focus();
     });
-    $('.search-field').blur();
   });
 
+  
+  $('#search-field').on('blur', function (){
+    $('.search-field').toggle()
+  });
+
+  // $('.main-navigation #search-field').hide();
+
+
+  // $('#search-submit').click(function () {
+  //   // e.preventDefault()
+  //   $('#search-field').toggle(function () {
+  // });
+
+
+  // });
+
+  // (function ($){
+  //   $('#search-toggle').on('click', function() {
+  //     $('#search-field').animate( { width: '200px' }, 500 ).focus();
+  //   });
+  //   $('#search-field').on('blur', function() {
+  //       $('#search-field').animate( { width: '0px', border:"none" }, 500 );
+  //   });
+  //   $('#search-field').animate( { width: 0 }, 0 );
+      
+  // })(jQuery);
+  
 
   // $('.main-navigation .search-submit')
   // .on('click', function(e){
@@ -17,9 +43,7 @@
   //   $('.main-navigation .search-field').toggle().focus(); 
   // });
 
-  // $('.main-navigation .search-field').on('blur', function() {
-  //   $(this).toggle();
-  // });
+})(jQuery);
+
   
 
-})(jQuery);
--- themes/inhabitent/searchform.php
+++ themes/inhabitent/searchform.php
@@ -1,18 +1,18 @@
 <span class="screen-reader-text"><?php echo esc_html( 'Search' ); ?></span>
 
 
-	<button class="search-submit">
+	<button id="search-submit" class="search-submit">
 		<span class="icon-search" aria-hidden="true">
 				<i class="fa fa-search"></i>
 		</span>
 
 	</button>
 
-<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+<form id="search-form" role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<fieldset>
 		
 		<label>
-			<input type="search" class="search-field" placeholder="type and hit enter..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
+			<input id="search-field" type="search" class="search-field" placeholder="type and hit enter..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		</label>
 		
 	</fieldset>
